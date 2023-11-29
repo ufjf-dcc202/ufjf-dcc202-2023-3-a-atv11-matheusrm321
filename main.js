c6498e8714a217f6df57ed8f0c8b0d6516fd0581
Matheus Ribeiro Menezes <matheusribeiro.menezes@estudante.ufjf.br>
2023-11-29 20:18:29 -0300

    função atualizarLista feita na main

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-import { getLista } from "./lista";
+import { getLista } from "./lista.js";
 
 const pEntrada = document.querySelector('#entrada');
 const olSaida = document.querySelector('#saida');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
-olSaida.innerHTML = "";
+
+atualizarItensDeLista();
+
+function atualizarItensDeLista()
+{
+    olSaida.innerHTML = "";
+    const lista = getLista();
+    for(let i = 0; i < lista.length; i++)
+    {
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
+    }
+}
